f7f3548b85cc4476e39e639740e13ee61402bc2f
Rolando de Guzman <deguzman.ollie@gmail.com>
2017-06-14 15:23:28 -0500

    Create ex8.js

---
+++ Start a Node.js server/ex8.js
@@ -0,0 +1,15 @@
+//Exercise 8
+var http = require("http");
+var bl = require("bl");
+
+var url = process.argv[2];
+
+http.get(url, function(response) {
+    response.pipe(bl(function(err, data) {
+        if (err)
+            console.log(err);
+        
+        console.log(data.length);
+        console.log(data.toString());
+    }))
+});
