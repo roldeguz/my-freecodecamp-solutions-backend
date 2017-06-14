77c243dfef5bd3a6f2bbb59e89a3d27c6bb22595
Rolando de Guzman <deguzman.ollie@gmail.com>
2017-06-14 14:03:08 -0500

    Create ex3.js

---
+++ Start a Node.js server/ex3.js
@@ -0,0 +1,8 @@
+//Exercise 3
+var fs = require("fs");
+
+var buff = fs.readFileSync(process.argv[2]);
+var sbuff = buff.toString();
+var abuff = sbuff.split("\n");
+var count = abuff.length - 1;
+console.log(count);
