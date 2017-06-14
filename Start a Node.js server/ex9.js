78052d38222320e58fe1f4f6f1f889f775e710f3
Rolando de Guzman <deguzman.ollie@gmail.com>
2017-06-14 16:44:05 -0500

    Create ex9.js

---
+++ Start a Node.js server/ex9.js
@@ -0,0 +1,36 @@
+//Exercise 9
+var http = require("http");
+var bl = require("bl");
+
+var urls = [];
+urls.push(process.argv[2]);
+urls.push(process.argv[3]);
+urls.push(process.argv[4]);
+
+var result = [];
+var done = 0;
+
+var fetch = function(u, i) {
+    http.get(u, function(response) {
+        response.pipe(bl(function(error, data) {
+            if (error)
+                console.log(error);
+            
+            result[i] = data.toString();
+            done++;
+            if (done == urls.length) {
+                print();
+            }
+        }))
+    });
+};
+
+var print = function () {
+    for (var i = 0; i < result.length; i++) {
+        console.log(result[i]);
+    }
+};
+
+urls.map(function (u, i) {
+    fetch(u, i);
+});
