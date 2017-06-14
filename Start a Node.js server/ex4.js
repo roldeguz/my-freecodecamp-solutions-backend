b5c05297ab2086435cf36a6ff8a89c39123d26ce
Rolando de Guzman <deguzman.ollie@gmail.com>
2017-06-14 14:03:44 -0500

    Create ex4.js

---
+++ Start a Node.js server/ex4.js
@@ -0,0 +1,11 @@
+//Exercise 4
+var fs = require("fs");
+
+fs.readFile(process.argv[2], function(err, data) {
+    
+    if (err)
+        throw err;
+    
+    var count = data.toString().split("\n").length - 1;
+    console.log(count);
+});
