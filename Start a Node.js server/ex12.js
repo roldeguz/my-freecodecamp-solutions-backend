00acdac97985a3e100557659770796345f9506c9
Rolando de Guzman <deguzman.ollie@gmail.com>
2017-06-15 13:52:22 -0500

    Create ex12.js

---
+++ Start a Node.js server/ex12.js
@@ -0,0 +1,17 @@
+//Exercise 12
+//http uppercaser
+
+var http = require("http");
+var map = require("through2-map");
+
+var port = process.argv[2];
+
+var server = http.createServer(function (request, response) {
+    if (request.method == 'POST') {
+        request.pipe(map(function (chunk) {
+            return chunk.toString().toUpperCase();
+        })).pipe(response);
+    }
+});
+
+server.listen(port);
