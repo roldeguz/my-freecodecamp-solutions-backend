e786468db12c1c0e951667bb99087ac5c5e7e26b
Rolando de Guzman <deguzman.ollie@gmail.com>
2017-06-15 13:06:51 -0500

    Create ex11.js

---
+++ Start a Node.js server/ex11.js
@@ -0,0 +1,16 @@
+//Exercise 11
+//http file server
+
+var http = require("http");
+var fs = require("fs");
+
+var port = process.argv[2];
+var loc = process.argv[3];
+
+var server = http.createServer(function(request, response) {
+    var stream = fs.createReadStream(loc);
+    
+    stream.pipe(response);
+});
+
+server.listen(port);
