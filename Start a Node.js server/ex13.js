942cb9f5f8ccd2827ecdc450eddf0d06dbad7c2e
Rolando de Guzman <deguzman.ollie@gmail.com>
2017-06-16 12:16:30 -0500

    Create ex13.js

---
+++ Start a Node.js server/ex13.js
@@ -0,0 +1,31 @@
+//Exercise 13
+//http json api server
+
+var http = require("http");
+var url = require("url");
+
+var port = process.argv[2];
+
+var server = http.createServer(function(request, response) {
+    if (request.method == 'GET') {
+        var info = url.parse(request.url, true);
+        var qdata = info.query;
+        var iso = qdata.iso;
+        var d = new Date(iso);
+        var result;
+        
+        switch (info.pathname) {
+            case '/api/parsetime':
+                result = {"hour": d.getHours(), "minute": d.getMinutes(), "second": d.getSeconds()};
+                break;
+                
+            case '/api/unixtime':
+                result = {"unixtime": d.getTime()};
+                break;
+                
+        }
+        
+        response.writeHead(200, { 'Content-Type': 'application/json' })
+        response.end(JSON.stringify(result))
+    }
+});
