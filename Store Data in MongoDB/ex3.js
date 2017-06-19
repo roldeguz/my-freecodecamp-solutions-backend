8fb76ce4e02c9d4e6dc30bb00bdd2c42505618b3
Rolando de Guzman <deguzman.ollie@gmail.com>
2017-06-19 15:21:59 -0500

    Update and rename ex1.js to ex3.js

---
+++ Store Data in MongoDB/ex3.js
@@ -0,0 +1,21 @@
+//Exercise 3
+//Find
+
+var mongo = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/learnyoumongo";
+var age = parseInt(process.argv[2]);
+
+mongo.connect(url, function(error, db) {
+    if (error) 
+        throw error;
+            
+    var coll = db.collection('parrots');
+    var result = coll.find({ age: { $gt: age }});
+    result.toArray(function(error, docs) {
+        if (error) 
+            throw error;
+            
+        console.log(docs);
+        db.close();
+    });
+});
