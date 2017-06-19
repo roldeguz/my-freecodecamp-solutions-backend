90706c99a7f4950333f3936dea69a2a3e24e9646
Rolando de Guzman <deguzman.ollie@gmail.com>
2017-06-19 15:28:35 -0500

    Create ex4.js

---
+++ Store Data in MongoDB/ex4.js
@@ -0,0 +1,22 @@
+//Exercise 4
+//Find project
+//To start mongo: mongod --port 27017 --dbpath=./data --smallfiles
+
+var mongo = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/learnyoumongo";
+var age = parseInt(process.argv[2]);
+
+mongo.connect(url, function(error, db) {
+    if (error) 
+        throw error;
+            
+    var coll = db.collection('parrots');
+    var result = coll.find({ age: { $gt: age }}, {_id: 0});
+    result.toArray(function(error, docs) {
+        if (error) 
+            throw error;
+            
+        console.log(docs);
+        db.close();
+    });
+});
