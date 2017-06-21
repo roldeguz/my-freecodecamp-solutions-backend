0c05cbc8224c8a74823301eddb71bcd7ee5909c7
Rolando de Guzman <deguzman.ollie@gmail.com>
2017-06-21 14:52:11 -0500

    Create ex8.js

---
+++ Store Data in MongoDB/ex8.js
@@ -0,0 +1,20 @@
+//Exercise 8
+//Count
+//To start mongo: mongod --port 27017 --dbpath=./data --smallfiles
+
+var mongo = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/learnyoumongo";
+var age = parseInt(process.argv[2]);
+
+mongo.connect(url, function(error, db) {
+   
+   var coll = db.collection('parrots');
+   
+   coll.count({age: { $gt: age }}, function(error, count) {
+        if (error)
+            throw error;
+            
+        console.log(count);
+        db.close();
+   });
+});
